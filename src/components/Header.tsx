dc6e402a6ce8ba4c74e8809f1a81c2d460718c48
Eduardo <edu-henrique2014@hotmail.com>
2022-07-20 21:53:41 -0300

    Header component modified

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import { useTheme, Heading, HStack, IconButton, StyledProps } from 'native-base';
+import { useTheme, Heading, HStack, IconButton, StyledProps, Box } from 'native-base';
 import { CaretLeft } from 'phosphor-react-native';
 
 interface HeaderProps extends StyledProps {
@@ -26,15 +26,17 @@ export function Header({ title, ...rest }: HeaderProps) {
     >
       <IconButton 
         icon={<CaretLeft size={24} color={colors.gray[200]} />}
+        rounded="full"
         ml={-2}
-        rounded="full"        
         _pressed={{ bg: "gray.500" }}
         onPress={handleGoBack}
       />
 
-      <Heading color="gray.100" textAlign="center" fontSize="lg" flex={1} ml={-10}>
+      <Heading color="gray.100" textAlign="center" fontSize="lg" flex={1}>
         {title}
       </Heading>
+
+      <Box w={12} />
     </HStack>
   );
 }
